cdb23c1708a9541a38987c32a7bc41f5d34101fa
miki-bgd <mimi2366@gmail.com>
2020-03-16 00:08:52 +0100

    Added dll files.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.3.0")]
 
 // In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
